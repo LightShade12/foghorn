afb3c71c4fbca6756dd0dafddb71b80e660c3e3b
Subham Swostik Pradhan <102753608+LightShade12@users.noreply.github.com>
2023-04-16 21:07:31 +0530

    movement upgrade #1

--- Foghorn/scenes/mainscreen_logic.gd
+++ Foghorn/scenes/mainscreen_logic.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var lerfv:float=0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -6,7 +7,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	lerfv+=1*delta
 	pass
 
 
@@ -16,5 +18,7 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://maps/test_map01.tscn")
+	$Control/CanvasLayer/CanvasModulate.color[3]=lerpf(1,0,lerfv)
+
+#	get_tree().change_scene_to_file("res://maps/test_map01.tscn")
 	pass # Replace with function body.
--- Foghorn/src/scripts/ent/player/player.gd
+++ Foghorn/src/scripts/ent/player/player.gd
@@ -2,14 +2,24 @@ extends character
 class_name player
 
 var dir=Vector3()
-
+#@onready var camera_holder=$head
+@onready var fpscam=$head/Camera3D
+@onready var p_col=$CollisionShape3D
+@onready var p_col_foot=$footcollision
 @export var env_file: Environment;
 
+#@onready var orig_cam_translation: Vector3 =camera_holder.position
+#var delta_=0;
+var crouching_speed=20;
+var def_height=1.7
+var crouch_height=0.85
+
+
+var camera_tilt = 0.4  #The speed which the camera will rotate back
 
 func _ready() -> void:
 	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
-	jump=7;
-#	gravity=40
+	jump=7;#override
 	if env_file!=null:
 		$head/Camera3D.set_environment(env_file)
 
@@ -22,22 +32,22 @@ func _input(event):
 
 
 func _process(_delta: float) -> void:
-
+	SPEED=default_move_speed
 	if Input.is_action_pressed("press_quit"):
 		get_tree().quit()
-#	if velocity.x!=0 and velocity.z!=0:
-#		audioplayer.playing=true
-#	else:
-#		audioplayer.playing=false
+
 
 func _physics_process(delta: float) -> void:
+#	delta_+=delta
 
-	dir=Vector3()
+	dir=Vector3()#reset dir length
+
 	if groundcheck.is_colliding():
 		full_contact=1
 	else:
 		full_contact=0
 
+#jump handling
 	if not is_on_floor():
 		gravity_vec+=Vector3.DOWN*gravity*delta
 		h_accel=air_accel
@@ -51,15 +61,45 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("press_jump") and (is_on_floor() or groundcheck.is_colliding()):
 		gravity_vec=Vector3.UP*jump
 
+#crouch handling
+	if Input.is_action_pressed("press_crouch"):
+		p_col_foot.transform.origin.y+=crouching_speed*delta
+		p_col.shape.height-=crouching_speed*delta
+
+		SPEED=default_crouch_speed
+
+	else:
+		p_col_foot.transform.origin.y-=crouching_speed*delta
+		p_col.shape.height+=crouching_speed*delta
+
+		SPEED=default_move_speed
+
+	p_col.shape.height=clamp(p_col.shape.height,crouch_height,def_height)
+	p_col_foot.transform.origin.y=clamp(p_col.transform.origin.y,0.422,1)
+
+# movement handling//USE "ELIF" IF DIAGONAL SPEED IS HIGH
 	if Input.is_action_pressed("move_front"):
 		dir-=transform.basis.z
 
-	elif Input.is_action_pressed("move_back"):
+	if Input.is_action_pressed("move_back"):
 		dir+=transform.basis.z
-	elif Input.is_action_pressed("move_right"):
+
+	if Input.is_action_pressed("move_right"):
+		fpscam.rotate_z(deg_to_rad(-camera_tilt))
 		dir+=transform.basis.x
-	elif Input.is_action_pressed("move_left"):
+
+	if Input.is_action_pressed("move_left"):
+		fpscam.rotate_z(deg_to_rad(camera_tilt))
 		dir-=transform.basis.x
+	#Returning the camera angle back to zero. The 0.5 has the camera rotate back at a slower rate.
+	if not Input.is_action_pressed("move_left"):
+		if fpscam.rotation.z > 0:
+			fpscam.rotate_z(deg_to_rad(-camera_tilt * 0.5))
+	if not Input.is_action_pressed("move_right"):
+		if fpscam.rotation.z < 0:
+			fpscam.rotate_z(deg_to_rad(camera_tilt * 0.5))
+
+	fpscam.rotation.z = clamp(fpscam.rotation.z , -0.05, 0.05)  #Changing the thresholds will change how far the camera will rotate in either direction
 
 	velocity=dir.normalized()*SPEED
 	h_velocity=h_velocity.lerp(dir*SPEED,h_accel*delta)
@@ -68,3 +108,8 @@ func _physics_process(delta: float) -> void:
 	velocity.y=gravity_vec.y
 
 	move_and_slide()
+#	print(dir)
+#	var camera_bob=floor(abs(velocity.x)+abs(velocity.z))*delta_*4 #dir=input?
+#	var target_cam_translation=orig_cam_translation+Vector3.UP*sin(camera_bob)*0.5
+#	camera_holder.position=camera_holder.position.lerp(target_cam_translation,delta)
+
